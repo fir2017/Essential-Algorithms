6810dd3aca11af457cdad3f062d625d23ffa2208
mcolic <medina.colic@gmail.com>
2016-09-05 19:28:04 -0500

    Update on gcd_step.py

--- algorithms/gcd_step.py
+++ algorithms/gcd_step.py
@@ -3,14 +3,18 @@ import random
 
 def gcd(a, b):
     count = 0
+    average = float((a+b)/2)
+
     while b:
         a, b = b, a%b
         count += 1
-        average = float((a+b)/2)
     return a, count, average
 
+print "Enter number of sets:"
 n = input()
 results = []
 for i in range(n):
     i =  gcd(random.randint(1,10000), random.randint(1,10000))
     results.append(i)
+print "(GCD, steps, average)"
+print results
